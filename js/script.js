06f491bb436996525f21e30fa28561c4ee611620
Jorge Sanchez <jorgesanchez@riberadeltajo.es>
2026-09-18 16:06:29 +0200

    Show Results

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+// Pide un numero y comprueba si son numeros
 const getNumber = () => {
     let num;
     num = parseInt(prompt("Introduce un número"));
@@ -9,22 +9,25 @@ const getNumber = () => {
     }
     return num;
 }
-
+//  Suma dos números
 const addNumbers = (num1, num2) => {
     let result = num1 + num2;
     return result;
 }
 
+//  Resta dos números
 const substractNumbers = (num1, num2) => {
     let result = num1 - num2;
     return result;
 }
 
+// 
 const multiplyNumbers = (num1, num2) => {
     let result = num1 * num2;
     return result;
 }
 
+//  Divide dos números
 const divideNumbers = (num1, num2) => {
     if (num2 === 0) {
         return "Error: No se puede dividir entre cero";
@@ -35,11 +38,18 @@ const divideNumbers = (num1, num2) => {
 
 }
 
+//  Muestra los resultados
+const showResults = (num1, num2) => {
+    console.log("El resultado de la suma es: " + addNumbers(num1, num2));
+    console.log("El resultado de la resta es: " + substractNumbers(num1, num2));
+    console.log("El resultado de la multiplicación es: " + multiplyNumbers(num1, num2));
+    console.log("El resultado de la división es: " + divideNumbers(num1, num2));
+}
 
+// Petición de números
 let num1 = getNumber();
 let num2 = getNumber();
 
-console.log("El resultado de la suma es: " + addNumbers(num1, num2));
-console.log("El resultado de la resta es: " + substractNumbers(num1, num2));
-console.log("El resultado de la multiplicación es: " + multiplyNumbers(num1, num2));
-console.log("El resultado de la división es: " + divideNumbers(num1, num2));
+// Mostrar resultados
+showResults(num1, num2);
+
