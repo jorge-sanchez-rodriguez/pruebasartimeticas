0789cabee7e7d8c3f0d920b58b15acf284fae60a
Jorge Sanchez <jorgesanchez@riberadeltajo.es>
2026-09-18 16:12:11 +0200

    Function multiply, don	 use operator *

--- js/script.js
+++ js/script.js
@@ -21,10 +21,13 @@ const substractNumbers = (num1, num2) => {
     return result;
 }
 
-// 
+//  Multiplica dos números
 const multiplyNumbers = (num1, num2) => {
-    let result = num1 * num2;
-    return result;
+    let product = num1;
+    for (let i = 1; i < num2; i++) {
+        product += num1;
+    }
+    return product;
 }
 
 //  Divide dos números
@@ -35,7 +38,6 @@ const divideNumbers = (num1, num2) => {
         let result = num1 / num2;
         return result;
     }
-
 }
 
 //  Muestra los resultados
